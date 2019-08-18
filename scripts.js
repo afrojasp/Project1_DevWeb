6185390d3097f534a5873c552ae7b122ded88d4e
andres felipe rojas pinzon <af.rojasp@uniandes.edu.co>
2019-08-18 11:41:23 -0500

    Corregido texto

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var football = document.getElementById("football-photo");
 var playstation = document.getElementById("playstation-photo");
 var programming = document.getElementById("programming-photo");
 
-alert("Hello, I recommend that you interact with the different elements of my web page, try to place the pointer on top of the elements and even some will react if you click them.");
+alert("Hello, I recommend you to interact with the different elements of my web page, try to place the pointer on top of the elements and even some will react if you click them.");
 
 football.onclick = function(){
   var src = football.getAttribute("src");
